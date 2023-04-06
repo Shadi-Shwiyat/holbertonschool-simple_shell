0bd3d208a8342aeab3818aeeb36f9183fe95a5d3
Nomad-Rob <rfarley89@gmail.com>
2023-04-06 12:53:02 -0700

    updated shell.h this is an infinite while loop that checks for EOF and tokenize input

--- shell.c
+++ shell.c
@@ -22,11 +22,26 @@ int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused))
 		/* return 1 if the file in the argument refers to the terminal */
 		status_return = isatty(STDIN_FILENO); /* checking if we are in interactive mode */
 		if (status_return) /* if we are in interactive mode */
-		{
 			write(STDOUT_FILENO, "$ ", 2); /* print prompt */
+		
+		glrv = getline(&input_stdin, &size, stdin); /* get input from stdin or check for EOF and error*/
+		if (glrv == -1) /* if we get EOF or any error */
+		{
+			free(input_stdin);
+			break;
 		}
+		if (validate_spaces(input_stdin)) /* if we get only spaces, tabs and line breaks */
+		{
+			free(input_stdin);
+			continue;
+		}
+		args = hsh_tokenizer(input_stdin); /* split input_stdin string into an array of arguments (tokenize input) */
+		if (*args[0] == '\0') /* if we get only spaces, tabs and line breaks */
+			continue;
+		status_return = hsh_execute_builtins(args, input_stdin,
+		argv, &exit_status);
+		free(input_stdin);
+		free(args);
 	}
-
-
-
+	return (0);
 }
